e6c27c32c96eade8fad3b5c55b7a4f8ad5d312f5
marcoluchi11 <marcoaluchi@hotmail.com>
2022-12-22 18:29:29 -0300

    add results when n/a its on the poster

--- components/SearchResult.jsx
+++ components/SearchResult.jsx
@@ -6,20 +6,27 @@ const SearchResult = ({ movies }) => {
   console.log(movies[0].imdbID);
   return (
     <section className="flex flex-wrap max-w-5xl items-center justify-around">
-      {movies.map((movie) => (
-        <div className="flex flex-col items-center" key={nanoid()}>
-          <Link href={`/title/${movie.imdbID}`}>
-            <Image
-              className="rounded-xl"
-              width={300}
-              height={300}
-              alt={movie.Title}
-              src={movie.Poster}
-            />
-            <h2 className="text-center text-white font-bold">{movie.Title}</h2>
-          </Link>
-        </div>
-      ))}
+      {movies.map((movie) => {
+        if (movie.Poster === "N/A") {
+          return null;
+        }
+        return (
+          <div className="flex flex-col items-center" key={nanoid()}>
+            <Link href={`/title/${movie.imdbID}`}>
+              <Image
+                className="rounded-xl hover:opacity-30"
+                width={300}
+                height={300}
+                alt={movie.Title}
+                src={movie.Poster}
+              />
+              <h2 className="text-center text-white font-bold">
+                {movie.Title}
+              </h2>
+            </Link>
+          </div>
+        );
+      })}
     </section>
   );
 };
