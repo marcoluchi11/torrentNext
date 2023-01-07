28d218c3625740abd9b876c43fd0b6bb696256b6
marcoluchi11 <marcoaluchi@hotmail.com>
2023-01-07 08:07:27 -0300

    add success alert

---
+++ components/Success.jsx
@@ -0,0 +1,12 @@
+const Success = () => {
+  return (
+    <div
+      className="bg-green-400 border text-center  text-black px-4 py-3 m-5 md:w-1/4 w-3/4 rounded"
+      role="alert"
+    >
+      <strong className="font-bold">Message sent successfully!</strong>
+    </div>
+  );
+};
+
+export default Success;
