72a8fa77286921f99f4027ea7df62a9c1106b79c
marcoluchi11 <marcoaluchi@hotmail.com>
2023-01-11 09:37:34 -0300

    change name for htmp properties for react

--- components/Form.jsx
+++ components/Form.jsx
@@ -52,9 +52,9 @@ const Form = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
-              class="w-5 h-5 mx-2"
+              className="w-5 h-5 mx-2"
             >
               <path
                 strokeLinecap="round"
