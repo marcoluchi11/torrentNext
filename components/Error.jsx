1cf1fa799455f48ea66411d994aecd7d636d64e4
marcoluchi11 <marcoaluchi@hotmail.com>
2023-01-16 19:12:24 -0300

    add different style to error component

--- components/Error.jsx
+++ components/Error.jsx
@@ -1,6 +1,6 @@
 const Error = ({ message }) => {
   return (
-    <div className="bg-red-500  text-center  text-black px-4 py-3 m-5 md:w-1/4 w-3/4 rounded">
+    <div className="bg-red-500  text-center  text-black px-4 py-3 my-2 md:w-1/4 w-3/4 rounded">
       <p>{message}</p>
     </div>
   );
