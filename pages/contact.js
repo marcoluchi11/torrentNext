96a76c875a1246f5de50ed8ca574a2cb6169b2e3
marcoluchi11 <marcoaluchi@hotmail.com>
2023-03-29 16:57:29 -0300

    delete icons

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,5 @@
 import Layout from "../components/Layout";
 import ContactForm from "./../components/ContactForm";
-import {
-  AiFillLinkedin,
-  AiOutlineMail,
-  AiOutlineTwitter,
-} from "react-icons/ai";
-import Link from "next/link";
 const Contact = () => {
   return (
     <Layout>
